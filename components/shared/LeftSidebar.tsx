c6982266d97a9a81d4612610020e176cabfe5b95
Cagan Pinar <cagan.pinar@hotmail.com>
2023-10-06 03:54:00 +0300

    Added profile section and fixed some visual bugs

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -7,9 +7,11 @@ import { link } from "fs";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { useAuth } from "@clerk/nextjs";
 function LeftSidebar() {
   const router = useRouter();
   const pathname = usePathname();
+  const { userId } = useAuth();
   return (
     <section className="custom-scrollbar leftsidebar">
       <div className="flex w-full flex-1 flex-col gap-6 px-6">
@@ -18,6 +20,8 @@ function LeftSidebar() {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
+
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
           return (
             <Link
               href={link.route}
